c21f0f168be37a439948c6c57d5e4ef3819d7637
ankita9809 <k.ankita037@gmail.com>
2022-06-17 23:06:50 +0530

    Weather

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,27 +9,11 @@ const memes = require("../controllers/memesController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-// ------------------------------------------------------- CoWin Data -----------------------------------------------------------------------------
-
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-router.get("/cowin/getDistrictsById", CowinController.getDistrictsById)
-
 
 //----------------------------------------------------------------- Weather Data -------------------------------------------------------------------
 
 router.get("/weather/getWeather", Weather.getWeather)
 router.get("/weather/getCity", Weather.getCity)
-
-
-//----------------------------------------------------------------- Memes Data ---------------------------------------------------------------------
-
-router.get("/memes/getAllMemes", memes.getAllMemes)
-router.post("/memes/memesHandler", memes.memesHandler)
 
 
 
